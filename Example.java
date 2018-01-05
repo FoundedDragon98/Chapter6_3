d22d81c2d7c4e1063c472d12e8152bd364d8e88d
FoundedDragon98 <31353323+FoundedDragon98@users.noreply.github.com>
2018-01-05 09:50:41 -0800

    1/5/18

---
+++ Example.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class Example
+{
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList();
+        ArrayList<String> arr2 = new ArrayList<String>();
+        arr2.add("Bye");
+        arr2.add("See you later");
+        arr.add(1);
+        arr.add("hi");
+        System.out.println(arr);
+    }
+}
